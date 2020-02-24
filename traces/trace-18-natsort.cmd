7c4b6d14ea618613ac6ae141938a7bc2510d7bb0
KY Weng <yaya573142@gmail.com>
2020-02-24 13:59:38 +0800

    Add a trace file to test the natural sort
    The trace-18-natsort.cmd can only be sorted correctly with natural sort.

---
+++ traces/trace-18-natsort.cmd
@@ -0,0 +1,29 @@
+# Test the correctness of natural sort
+option fail 0
+option malloc 0
+new
+it fred
+it pic2
+it pic100a
+it pic120
+it pic121
+it jane
+it tom
+it pic02a
+it pic3
+it pic4
+it 1-20
+it pic100
+it pic02000
+it 10-20
+it 1-02
+it 1-2
+it x2-y7
+it x8-y8
+it x2-y08
+it x2-g8
+it pic01
+it pic02
+it pic05
+sort
+show
